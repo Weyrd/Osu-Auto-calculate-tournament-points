f9df1f78abb2790dd1b85890e97aaababb7db61c
Weyrd <wyrd.reisa@gmail.com>
2022-03-23 19:15:31 +0100

    uncomment usefull stuff

--- js/main.js
+++ js/main.js
@@ -513,6 +513,7 @@ function reset() {
 
 function discord() {
   //msg = "Remplacez les mots 'MAP_BAN' et 'NUMBER' par les bonnes valeurs à la main :\n"
+  msg = ""
   msg += "\n**RO16 - " + lobby + "**"
   msg += "\n" + team["lobby"][lobby]["1"] + " | " + pointBlueVar + "-" + pointRedVar + " | " + team["lobby"][lobby]["2"]
   msg += "\n"
